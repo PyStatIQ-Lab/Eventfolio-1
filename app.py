104f59d90e2fa1a6a31d10dd68f2e336a42bdeea
PyStatIQ-Lab <support@pystatiq.com>
2025-04-27 13:22:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ st.markdown("""
         color: #dc3545;
     }
     .stock-card {
-        background-color: white;
+        background-color: blue;
         border-radius: 10px;
         padding: 15px;
         box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
